cfee88a26c8a71acfaf6082d22d12ccb1c47e495
reinoptland <reinoptland@gmail.com>
2016-08-31 20:51:55 +0200

    Create resources for projects, nest routes todos in projects

--- db/migrate/20160831184318_create_projects.rb
+++ db/migrate/20160831184318_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
       t.string :project_name
-      t.boolean :project_active
+      t.boolean :project_active, default: true
 
       t.timestamps
     end
